3374f27bdc30bd721cfab89cc15baf6392dae854
Antti <antti@antti.codes>
2022-10-06 21:17:25 +0300

    fixes and features
     - require cursor position change for heartbeat
    
     - add settings to configurations instead of global storage
    
     - use secret store for apikey
    
     - update deps
    
     - setting to disable testaustime
    
     - setting to hash project names in specified paths
    
     - async flush on exit
    
     - maybe other fixes

--- src/utils/timeUtils.ts
+++ src/utils/timeUtils.ts
@@ -12,14 +12,13 @@ export const prettyDuration = (seconds: number) => formatDuration(
   intervalToDuration({ start: 0, end: Math.round((seconds || 0) * 1000 / 60000) * 60000 }),
   {
     locale: {
-      // Let's just hope the token is one of these options
       formatDistance: (token: "xSeconds" | "xMinutes" | "xHours" | "xDays", count: number) => {
         if (!(token in formatShort)) {
           console.warn("Unimplemented token", token);
           return "";
         }
 
-        return formatShort[token].replace("{{count}}", String(count));
+        return formatShort[token].replace("{{count}}", count.toString());
       }
     }
   }) || "0s";
